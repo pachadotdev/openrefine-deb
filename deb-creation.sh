62be42b204e24d494a0af850a64f3e64267accfa
Mauricio Vargas <mvargas@dcc.uchile.cl>
2020-03-30 01:08:57 -0300

    sync

--- deb-creation.sh
+++ deb-creation.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ ! -d openrefine-3.2 ]; then
-	wget https://github.com/OpenRefine/OpenRefine/releases/download/3.2/openrefine-linux-3.2.tar.gz
-	tar -zxvf openrefine-linux-3.2.tar.gz
+if [ ! -d openrefine-3.3 ]; then
+	wget https://github.com/OpenRefine/OpenRefine/releases/download/3.3/openrefine-linux-3.3.tar.gz
+	tar -zxvf openrefine-linux-3.3.tar.gz
 	
-	mkdir openrefine-3.2/icons
-	cp icon.png openrefine-3.2/icons/openrefine.png
+	mkdir openrefine-3.3/icons
+	cp icon.png openrefine-3.3/icons/openrefine.png
 fi
 
-if [ -d openrefine-3.2 ]; then
+if [ -d openrefine-3.3 ]; then
 	echo "Packaging OpenRefine"
 	rm -rf deb-package
 	rm openrefine.deb
 	mkdir deb-package
 	mkdir -p deb-package/opt/openrefine
-	rsync -av --progress openrefine-3.2/* deb-package/opt/openrefine/ --exclude-from=deb-exclude
+	rsync -av --progress openrefine-3.3/* deb-package/opt/openrefine/ --exclude-from=deb-exclude
 	rsync -av --progress DEBIAN/ deb-package/DEBIAN/
 	chmod -R 0755 deb-package
 	dpkg-deb --build deb-package
-	mv deb-package.deb openrefine-3.2.deb
+	mv deb-package.deb openrefine-3.3.deb
 fi
